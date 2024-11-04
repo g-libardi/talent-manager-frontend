65eaea982fca336e8621b0cb5a0d6dc243867e05
g-libardi <contato@libardi.dev.br>
2024-11-04 16:46:05 -0300

    feat: add user info header to sidebar

--- app/src/components/DefaultSidebar.tsx
+++ app/src/components/DefaultSidebar.tsx
@@ -9,8 +9,11 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
-import { HomeIcon, LightbulbIcon } from "lucide-react"
-import { Link } from "react-router-dom"
+import { HomeIcon, LightbulbIcon, UserIcon } from "lucide-react"
+import { Link, useNavigate } from "react-router-dom"
+import { Avatar, AvatarFallback } from "./ui/avatar"
+import useAuth from "@/hooks/useAuth"
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar"
 
 
 const menuItems = [
@@ -28,27 +31,53 @@ const menuItems = [
 
 
 export function DefaultSidebar() {
+  const userInfo = useAuth().getUserInfo()
+  const navigate = useNavigate()
   return (
     <Sidebar>
-      <SidebarHeader />
+
+      <SidebarHeader className="pb-0 pl-0">
+        <Menubar className="bg-transparent border-0">
+          <MenubarMenu>
+            <MenubarTrigger asChild className="w-full rounded-xl hover:bg-secondary">
+              <div className="flex items-center gap-3">
+                <Avatar>
+                  <AvatarFallback className=" shadow-[inset_0_0px_10px_rgba(0,0,0,0.4)]">
+                    <span className="font-bold">
+                      <UserIcon/>
+                    </span>
+                  </AvatarFallback>
+                </Avatar>
+                <div>
+                  <p>{userInfo?.username}</p>
+                </div>
+              </div>
+            </MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem onClick={async() => useAuth().logout().then(() => navigate('/login'))}>
+                Logout
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+      </SidebarHeader>
+
       <SidebarContent>
-        <SidebarGroup />
+        <SidebarGroup>
           <SidebarGroupLabel>Talent Manager</SidebarGroupLabel>
-          <SidebarContent>
-            <SidebarMenu>
-              {menuItems.map((item, index) => (
-                <SidebarMenuItem key={index}>
-                  <SidebarMenuButton asChild>
-                    <Link to={item.to}>
-                      <item.icon />
-                      {item.title}
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarContent>
-        <SidebarGroup />
+          <SidebarMenu>
+            {menuItems.map((item, index) => (
+              <SidebarMenuItem key={index}>
+                <SidebarMenuButton asChild>
+                  <Link to={item.to}>
+                    <item.icon />
+                    {item.title}
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>

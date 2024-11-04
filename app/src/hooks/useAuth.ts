7277d40ad6a3370ac623644d1441c5fe0bc27257
g-libardi <contato@libardi.dev.br>
2024-11-04 14:00:38 -0300

    fix: fix usage of storage hook in useAuth

--- app/src/hooks/useAuth.ts
+++ app/src/hooks/useAuth.ts
@@ -1,8 +1,8 @@
 
 import axios from 'axios';
 import useToast from './useToast';
+import useStore from './useStore';
 
-const toast = useToast();
 
 export interface Credentials {
     username: string;
@@ -10,37 +10,53 @@ export interface Credentials {
 }
 
 
-async function login(credentials: Credentials) {
-    try {
-        const response = axios.post('/api/login/token-pair', credentials);
-        toast.promise(response, 'Login successful!', 'Login failed, verify your credentials.');
-        return (await response).data;
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
-
-
-async function refreshToken() {
-    try {
-        const response = await axios.post('/api/login/access-token');
-        return response.data;
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
-
-
-async function logout() {
-    try {
-        await axios.delete('/api/login/token-pair');
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
-
-
 export default function useAuth() {
+    const toast = useToast();
+    const store = useStore.setState;
+
+    async function login(credentials: Credentials) {
+        try {
+            const response = axios.post('/api/login/token-pair', credentials);
+            toast.promise(response, 'Login successful!', 'Login failed, verify your credentials.');
+            const data = (await response).data;
+            store((state) => ({
+               auth: {
+                    ...state.auth,
+                    accessToken: data.access,
+                    refreshToken: data.refresh,
+               }
+            }));
+            console.log(data);
+        } catch (error) {
+            store((state) => ({
+                auth: {
+                    ...state.auth,
+                    accessToken: null,
+                    refreshToken: null,
+                }
+            }));
+            return Promise.reject(error);
+        }
+    }
+
+
+    async function refreshToken() {
+        try {
+            const response = await axios.post('/api/login/access-token');
+            return response.data;
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+
+
+    async function logout() {
+        try {
+            await axios.delete('/api/login/token-pair');
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
     return {
         login,
         refreshToken,

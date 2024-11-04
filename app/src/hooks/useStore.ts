2699f23ddade44723e9703d52cf913e1df71df5f
g-libardi <contato@libardi.dev.br>
2024-11-04 13:57:55 -0300

    fix: fix useStore typing

--- app/src/hooks/useStore.ts
+++ app/src/hooks/useStore.ts
@@ -6,8 +6,8 @@ interface AppState {
     auth: {
         accessToken: Object | null;
         refreshToken: Object | null;
-        setAccessToken: (accessToken: Object) => void;
-        setRefreshToken: (refreshToken: Object) => void;
+        setAccessToken: (accessToken: Object | null) => void;
+        setRefreshToken: (refreshToken: Object | null) => void;
     },
 }
 

6405c5c67e1611a4a6748f616d76860ea8099ff0
g-libardi <contato@libardi.dev.br>
2024-11-04 09:16:41 -0300

    feat: render toasts on main

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import routes from '@/plugins/fileBasedRoutes'
+import { Toaster } from '@/components/ui/sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,5 +16,6 @@ createRoot(document.getElementById('root')!).render(
         ))}
       </Routes>
     </BrowserRouter>
+    <Toaster />
   </StrictMode>,
 )

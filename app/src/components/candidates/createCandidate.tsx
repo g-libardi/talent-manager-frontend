98e8e6afe2318ff60c23733fa9d10725151d08af
g-libardi <contato@libardi.dev.br>
2024-11-05 00:36:16 -0300

    feat: add create candidate modal

---
+++ app/src/components/candidates/createCandidate.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import CandidateCreationForm from "./candidateCreationForm"
+
+export default function CreateCandidate() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add Candidate</Button>
+      </DialogTrigger>
+
+
+      <DialogContent className="max-w-[90%] max-h-[90%] overflow-auto">
+        <DialogHeader>
+          <DialogTitle>Create candidate</DialogTitle>
+          <DialogDescription>
+            Fill the form and click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+
+        <CandidateCreationForm />
+
+      </DialogContent>
+    </Dialog>
+  )
+}
+
